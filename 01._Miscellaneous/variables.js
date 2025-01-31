4aedc577f712827531f0a31c82102d169b94b7b3
anderslatif <anderslatif@users.noreply.github.com>
2025-01-31 10:34:06 +0100

    Added the variables file

---
+++ 01._Miscellaneous/variables.js
@@ -0,0 +1,60 @@
+
+// avoid var 
+// var name = 'Anders';
+
+// const name = 'Anders';
+
+// const is NOT a constant
+// that means that const CAN have its value changed
+// const is a constant in the DECLARATION
+// const MUST be declared as it is initialized
+
+// name = 'Thea';
+
+const me = {};
+
+me.name = "Anders";
+
+console.log(typeof me.name);
+
+
+const hobbies = ["sleeping", "coding"];
+
+hobbies.pop();
+
+hobbies.push("drawing", "painting");
+
+console.log(hobbies);
+
+
+const jsObject = {
+    whatAmI: "jsObject",
+    // comment
+};
+
+// " vs. ' vs. `
+
+const dancerName = "Computer";
+
+// "" or '' or `` = string literals
+console.log("I can't dance!");
+console.log('I can "dance"!');
+// ` (backticks)
+// `` = template strings
+// ${} syntax = string interpolation
+console.log(`${dancerName} can dance '"""''
+    line 1
+    line 2    
+    '""''''"""''`);
+
+
+
+// console.log , vs. + 
+
+const assignmentDescription = "...and the value is...";
+const value = 4;
+
+// don't use plus in console.log to avoid type coercion
+console.log(assignmentDescription, value);
+
+
