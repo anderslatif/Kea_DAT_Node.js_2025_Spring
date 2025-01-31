25fe513cc8f79b58264916de1a212be101dc392c
anderslatif <anderslatif@users.noreply.github.com>
2025-01-31 12:26:55 +0100

    Solved the first exercise

--- 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/1.Basics.js
+++ 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/1.Basics.js
@@ -9,6 +9,11 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+const introduction = `My first name is ${firstName} ${lastName}`;
+
+console.log(introduction);
+
+// ASI
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -20,5 +25,17 @@ const increment = 1;
 // The result should be 2025
 // You cannot touch the first or the second line
 
+// console.log(Number(year) + increment);
+// console.log(parseInt(year) + increment);
+// console.log(+year + increment);
+
+
+const isItANumber = "3424dsf";
+
+console.log(Number(isItANumber));
+console.log(parseInt(isItANumber));
+
+
+// todo next week datatypes
 
 // --------------------------------------
