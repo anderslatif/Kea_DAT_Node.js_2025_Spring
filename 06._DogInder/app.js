7f632766fb4e981b9fbd0befeee108c4c26476f5
anderslatif <anderslatif@users.noreply.github.com>
2025-03-14 10:07:02 +0100

    Started making the SSR sandwich

--- 06._DogInder/app.js
+++ 06._DogInder/app.js
@@ -9,16 +9,21 @@ import fs from 'fs';
 
 import { getMatches } from './util/matches.js';
 
+const header = fs.readFileSync('./public/components/header/header.html').toString();
+const footer = fs.readFileSync('./public/components/footer/footer.html').toString();
+
 const frontpage = fs.readFileSync('./public/frontpage/frontpage.html').toString();
 const matches = fs.readFileSync('./public/matches/matches.html').toString();
 
+const frontpagePage = header + frontpage + footer;
+const matchesPage = header + matches + footer;
 
 app.get("/", (req, res) => {
-    res.send(frontpage);
+    res.send(frontpagePage);
 });
 
 app.get("/matches", (req, res) => {
-    res.sendFile(path.resolve('public/matches/matches.html'));
+    res.send(matchesPage);
 });
 
 app.get("/api/matches", async (req, res) => {
