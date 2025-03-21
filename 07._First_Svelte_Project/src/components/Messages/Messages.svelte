7a881076b0621ffb89f7505cd130b005f5dff571
anderslatif <anderslatif@users.noreply.github.com>
2025-03-21 10:57:06 +0100

    Created the messages component

---
+++ 07._First_Svelte_Project/src/components/Messages/Messages.svelte
@@ -0,0 +1,19 @@
+<script>
+    const messages = $state([]);
+
+    let messageInput = $state("");
+
+    function addMessage() {
+        messages.push(messageInput);
+        messageInput = "";
+    }
+</script>
+
+<h1>Messages</h1>
+
+<p>{messages}</p>
+
+<input bind:value={messageInput} placeholder="Type your message...">
+
+<button onclick={addMessage}>Add Message</button>
+
