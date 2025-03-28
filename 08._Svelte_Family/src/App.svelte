bd92702fc6ad22b8e33a541deb65b09024077492
anderslatif <anderslatif@users.noreply.github.com>
2025-03-28 09:07:14 +0100

    Started the Svelte Family project

---
+++ 08._Svelte_Family/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Home from "./components/Home/Home.svelte";
+
+  const parentNames = ["Adam", "Eva"];
+  const children = [
+    {
+      name: "Kain",
+      familySheep: "black-sheep"
+    },
+    {
+      name: "Abel",
+      familySheep: "grey-sheep"
+    }, 
+    {
+      name: "Leona",
+      isGirl: true
+    }
+  ];
+
+</script>
+
+<Home>
+  <h1>Hello? Anyone in there?</h1>
+</Home>
---
+++ 08._Svelte_Family/src/components/Home/Home.svelte
@@ -0,0 +1,13 @@
+
+
+<slot></slot>
+
+<h3 id="welcome-mat">Home sweet home</h3>
+
+<style>
+    #welcome-mat {
+        background-color: rgba(165, 42, 42, 0.381);
+        padding: 20px;
+        border-radius: 0 0 12px 12px;
+    }
+</style>
