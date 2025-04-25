5581756ec7903c86c28acff6aab792ec633c398c
anderslatif <anderslatif@users.noreply.github.com>
2025-04-25 10:22:44 +0200

    Finished the seeding of the database

--- 11._Games/database/createDatabase.js
+++ 11._Games/database/createDatabase.js
@@ -1,30 +1,41 @@
 import db from './connection.js';
 
+// db.all for SELECT statements, we want to get something back
+// db.run for INSERT, UPDATE, DELETE, nothing gets return
+// db.exec for schematics without parameters, can contain multiple commands
+
 const deleteMode = process.argv.includes("--delete");
 
 if (deleteMode) {
-    db.run(`DROP TABLE IF EXISTS games;`);
-    db.run(`DROP TABLE IF EXISTS runtime_environments;`);
+    await db.run(`DROP TABLE IF EXISTS games;`);
+    await db.run(`DROP TABLE IF EXISTS runtime_environments;`);
 }
 
 
 // DDL
-db.exec(`CREATE TABLE IF NOT EXISTS runtime_environments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    platform TEXT,
-    version TEXT
-);
-`);
+await db.exec(`
+    CREATE TABLE IF NOT EXISTS runtime_environments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        platform TEXT,
+        version TEXT
+    );
 
-db.exec(
-    `CREATE TABLE IF NOT EXISTS games (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    short_description VARCHAR(500),
-    genre TEXT CHECK( genre IN ('MMO', 'RPG', 'FPS') ),
-    runtime_environment_id INTEGER,
-    FOREIGN KEY (runtime_environment_id) REFERENCES runtime_environments (id)
-);
+    CREATE TABLE IF NOT EXISTS games (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        short_description VARCHAR(500),
+        genre TEXT CHECK( genre IN ("MMO", "RPG", "FPS") ),
+        runtime_environment_id INTEGER,
+        FOREIGN KEY (runtime_environment_id) REFERENCES runtime_environments (id)
+    );
 `);
 
 // DML
+// seeding
+if (deleteMode) {
+    await db.run('INSERT INTO runtime_environments (platform, version) VALUES ("Linux", "Arch");');
+    await db.run('INSERT INTO games (title, runtime_environment_id) VALUES ("Tux Kart", 1);')
+    await db.run('INSERT INTO games (title) VALUES ("Hogwarts Legacy");')    
+}
+
+
